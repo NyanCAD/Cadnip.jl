bd18e9c4b5f633ac45d6380107a280e8f881ac9a
Pepijn de Vos <me@pepijndevos.nl>
2026-01-11 12:34:49 +0100

    Disable precompilation for BSIM-CMG model

--- VerilogAParser.jl/src/VerilogAParser.jl
+++ VerilogAParser.jl/src/VerilogAParser.jl
@@ -3,10 +3,12 @@ include("tokenize/VerilogATokenize.jl")
 include("parse/VerilogACSTParser.jl")
 using .VerilogACSTParser: parse, parsefile, EXPR, Node
 
+#=
 using SnoopPrecompile
 @precompile_all_calls begin
     # Use BSIM-CMG from CMCModels package location
     parsefile(joinpath(@__DIR__, "../../models/CMCModels.jl/va/bsimcmg.va"))
 end
+=#
 
 end
